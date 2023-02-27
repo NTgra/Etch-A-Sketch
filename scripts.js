9a2d97354f45a2fb424a11ef8540e2fad5afafab
NTgra <nektotigra@gmail.com>
2023-02-27 15:28:52 -0700

    Add the button to choose the Grid Size

--- scripts.js
+++ scripts.js
@@ -1,25 +1,48 @@
 
-const container = document.querySelector('#container');
-
+function clearBox(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+const wrapper = document.querySelector('#wrapper');
 function displayDivs(grid){
-
+    const container = document.querySelector('#container');
+    
+    let divWidth=((640/grid));
+    console.log(divWidth)
+    
     let j=0;
     while(j<grid){
         for (i=0; i<grid; i++){
             const canvas = document.createElement('div');
             canvas.classList.add('canvas-blank');
-            canvas.textContent = i;
-            container.appendChild(canvas);        
+            canvas.style.width=divWidth+"px";
+            canvas.style.height=divWidth+"px";
+            container.appendChild(canvas);              
         }
         j++;
-    }       
-}
-displayDivs(16);
+    } 
+    
+    let divFiller = document.querySelectorAll('.canvas-blank');
 
-let divFiller = document.querySelectorAll('.canvas-blank');
-
-for (let k=0; k<divFiller.length; k++){
-    divFiller[k].addEventListener('mouseover', function(e){
-        this.setAttribute("class", "canvas-blank-fill");
-    })
+  for (let k=0; k<divFiller.length; k++){
+      divFiller[k].addEventListener('mouseover', function(e){
+          this.setAttribute("class", "canvas-blank-fill");
+      })
+  }      
 }
+
+btn.addEventListener('click', () =>  {
+    let gridSize = prompt('');
+    if (gridSize>100){
+        alert('The grid size can\'t be bigger than 100x100\nSetting the grid to 100x100')
+        gridSize=100;
+    } 
+    if (gridSize<=0){
+        alert('The grid size can\'t be negative or zero!\nSetting the grid to 2x2')
+        gridSize=2;
+    }
+    clearBox('container');
+    displayDivs(gridSize); 
+  });
+
+  
