be8a6f74401f6d189774dd40d821a1b608cedee6
NTgra <nektotigra@gmail.com>
2023-02-28 12:28:10 -0700

    Add status bar

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ const wrapper = document.querySelector('#wrapper');
 function displayDivs(grid){
     const container = document.querySelector('#container');
     
-    let divWidth=((640/grid));
+    let divWidth=(640/grid);
     console.log(divWidth)
     
     let j=0;
@@ -17,11 +17,16 @@ function displayDivs(grid){
             canvas.classList.add('canvas-blank');
             canvas.style.width=divWidth+"px";
             canvas.style.height=divWidth+"px";
+            // add here the Math.random for RGB colors
             container.appendChild(canvas);              
         }
         j++;
+         
     } 
-    
+        const statusBar = document.createElement('div');
+        statusBar.classList.add('status-bar');
+        statusBar.textContent='Grid size: '+grid+'x'+grid;
+        container.appendChild(statusBar);  
     let divFiller = document.querySelectorAll('.canvas-blank');
 
   for (let k=0; k<divFiller.length; k++){
@@ -32,17 +37,17 @@ function displayDivs(grid){
 }
 
 btn.addEventListener('click', () =>  {
-    let gridSize = prompt('');
+     gridSize = prompt('');
     if (gridSize>100){
         alert('The grid size can\'t be bigger than 100x100\nSetting the grid to 100x100')
         gridSize=100;
     } 
     if (gridSize<=0){
-        alert('The grid size can\'t be negative or zero!\nSetting the grid to 2x2')
-        gridSize=2;
+        alert('The grid size can\'t be negative or zero!\nSetting the grid to 16x16')
+        gridSize=16;
     }
     clearBox('container');
-    displayDivs(gridSize); 
-  });
+    displayDivs(gridSize);
 
+  });
   
